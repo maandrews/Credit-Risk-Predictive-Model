92f98cb1f3e450f48b920f23e1da2b8ab1c972db
maandrews <31116472+maandrews@users.noreply.github.com>
2017-09-04 22:16:36 -0400

    Update CreditRisk.py

--- CreditRisk.py
+++ CreditRisk.py
@@ -15,7 +15,7 @@ from sklearn.metrics import roc_auc_score
 # machine learning
 from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, RandomForestRegressor, AdaBoostRegressor
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -37,6 +37,8 @@ combine = [train, test]
 combine[0] = combine[0].drop(["Unnamed: 0"], axis=1)
 combine[1] = combine[1].drop(["SeriousDlqin2yrs"], axis=1)
 
+# The training set has a few people with age=0.  I found the best way to deal with them is to just drop them.
+# Other approaches were to give them median age of people with same dependents or median of all ages.
 combine[0] = combine[0][combine[0].age != 0]
 
 # Replace NaNs with something else.  For monthly income, the median income of those with the same age.  Same for number
@@ -45,6 +47,7 @@ guess_income = np.zeros(int(combine[0]['age'].max()+1))
 guess_dependents = np.zeros(int(combine[0]['age'].max()+1))
 
 for i in range (0, combine[0]['age'].max()+1):
+    combine[0]['MonthlyIncome'].replace(1, np.NaN)
     income_df = combine[0][(combine[0]['age'] == i)]['MonthlyIncome'].dropna()
     dependents_df = combine[0][(combine[0]['age'] == i)]['NumberOfDependents'].dropna()
 
@@ -63,23 +66,35 @@ for i in range (0, combine[0]['age'].max()+1):
 
 for ds in combine:
     for i in range (0, ds['age'].max()+1):
-        ds.loc[(ds.MonthlyIncome.isnull()) & (ds.age == i),'MonthlyIncome'] = guess_income[i]
+        ds.loc[((ds.MonthlyIncome.isnull()) | (ds.MonthlyIncome == 1)) & (ds.age == i),'MonthlyIncome'] = guess_income[i]
         ds.loc[(ds.NumberOfDependents.isnull()) & (ds.age == i), 'NumberOfDependents'] = guess_dependents[i]
 
     ds['MonthlyIncome'] = ds['MonthlyIncome'].astype(int)
     ds['NumberOfDependents'] = ds['NumberOfDependents'].astype(int)
 
 
+
 for ds in combine:
+    # Extract an approximation of monthly debt payments
     ds['MonthlyDebt'] = ds.DebtRatio * ds.MonthlyIncome
+    ds['Income-Dep-RatioPre'] = ds.MonthlyIncome / (ds.NumberOfDependents + 1)
+    ds['Income-NumLoans-RatioPre'] = ds.MonthlyIncome / (ds.NumberOfOpenCreditLinesAndLoans +
+                                                      ds.NumberRealEstateLoansOrLines + 1)
+
+combine[0]['MonthlyDebt'] = np.log1p(combine[0]['MonthlyDebt'])
+combine[1]['MonthlyDebt'] = np.log1p(combine[1]['MonthlyDebt'])
+
+combine[0]['Income-Dep-RatioPre'] = np.log1p(combine[0]['Income-Dep-RatioPre'])
+combine[0]['Income-NumLoans-RatioPre'] = np.log1p(combine[0]['Income-NumLoans-RatioPre'])
+combine[1]['Income-Dep-RatioPre'] = np.log1p(combine[1]['Income-Dep-RatioPre'])
+combine[1]['Income-NumLoans-RatioPre'] = np.log1p(combine[1]['Income-NumLoans-RatioPre'])
+
 
 # Expect some data to be normally distributed, log transform them... also keeps model inputs from getting too high
 combine[0]['MonthlyIncome'] = np.log1p(combine[0]['MonthlyIncome'])
 combine[0]['age'] = np.log1p(combine[0]['age'])
 combine[1]['MonthlyIncome'] = np.log1p(combine[1]['MonthlyIncome'])
 combine[1]['age'] = np.log1p(combine[1]['age'])
-combine[0]['MonthlyDebt'] = np.log1p(combine[0]['MonthlyDebt'])
-combine[1]['MonthlyDebt'] = np.log1p(combine[1]['MonthlyDebt'])
 
 
 # Creating some features that improve performance.
@@ -88,9 +103,15 @@ for ds in combine:
     ds['Income-Dep-Ratio'] = ds.MonthlyIncome / (ds.NumberOfDependents + 1)
     ds['Income-NumLoans-Ratio'] = ds.MonthlyIncome / (ds.NumberOfOpenCreditLinesAndLoans +
                                                       ds.NumberRealEstateLoansOrLines + 1)
+
     # Create a weighting of how late someone has been in the past. Later payment = higher weight
-    ds['WeightedMissedPayments'] = 0.5*ds.NumberOfTimes90DaysLate + 0.3*ds['NumberOfTime60-89DaysPastDueNotWorse'] \
+    ds['WeightedMissedPayments'] = 3*ds.NumberOfTimes90DaysLate + 1.5*ds['NumberOfTime60-89DaysPastDueNotWorse'] \
                                    + 0.2*ds['NumberOfTime30-59DaysPastDueNotWorse']
+
+
+    # Some debt ratios are messed up and pretty high, scale them down to around what they should be
+    ds.loc[(ds.DebtRatio >= 2), 'DebtRatio'] = ds.DebtRatio / 10000
+
 
 
 # log transform other columns as it improves performance having smaller inputs.  Also not unreasonable to assume they
@@ -111,6 +132,7 @@ combine[1]['NumberOfOpenCreditLinesAndLoans'] = np.log1p(combine[1]['NumberOfOpe
 combine[1]['NumberRealEstateLoansOrLines'] = np.log1p(combine[1]['NumberRealEstateLoansOrLines'])
 combine[1]['NumberOfDependents'] = np.log1p(combine[1]['NumberOfDependents'])
 combine[1]['WeightedMissedPayments'] = np.log1p(combine[1]['WeightedMissedPayments'])
+
 
 # Training
 x_all = combine[0].drop("SeriousDlqin2yrs", axis=1)
@@ -137,6 +159,7 @@ dt.fit(x_train, y_train)
 
 # Neural Net
 nn = MLPClassifier(hidden_layer_sizes=(7,4), random_state=22)
+nn.fit(x_train,y_train)
 nn.fit(x_train,y_train)
 # predictions_NN = nn.predict_proba(x_2)
 # predictions_NN = predictions_NN[:, 1]
@@ -170,6 +193,10 @@ gnb.fit(x_train,y_train)
 # predictions_GNB = predictions_GNB[:, 1]
 # print("Gaussian Naive-Bayes:", roc_auc_score(y_2, predictions_GNB))
 
+# RF regressor
+rfreg = RandomForestRegressor(n_estimators = 200, random_state=12)
+rfreg.fit(x_train,y_train)
+
 # **Best solo performer from above is AdaBoost with AUROC of ~0.8612. A close second was the MLP, with AUROC of ~0.8574
 # on the validation set.
 
@@ -177,6 +204,7 @@ gnb.fit(x_train,y_train)
 predictions_LR_train = logreg.predict_proba(x_2)[:,1]
 predictions_DT_train = dt.predict_proba(x_2)[:,1]
 predictions_NN_train = nn.predict_proba(x_2)[:,1]
+predictions_RFR_train = rfreg.predict(x_2)
 predictions_KNN_train = knn.predict_proba(x_2)[:,1]
 predictions_RF_train = rf.predict_proba(x_2)[:,1]
 predictions_AB_train = ab.predict_proba(x_2)[:,1]
@@ -186,6 +214,7 @@ predictions_GNB_train = gnb.predict_proba(x_2)[:,1]
 predictions_LR_train = predictions_LR_train.reshape(-1, 1)
 predictions_DT_train = predictions_DT_train.reshape(-1, 1)
 predictions_NN_train = predictions_NN_train.reshape(-1, 1)
+predictions_RFR_train = predictions_RFR_train.reshape(-1, 1)
 predictions_KNN_train = predictions_KNN_train.reshape(-1, 1)
 predictions_RF_train = predictions_RF_train.reshape(-1, 1)
 predictions_AB_train = predictions_AB_train.reshape(-1, 1)
@@ -193,7 +222,7 @@ predictions_GNB_train = predictions_GNB_train.reshape(-1, 1)
 
 # What to train the meta model on
 next_x_train = np.concatenate((predictions_LR_train,predictions_DT_train, predictions_NN_train, predictions_KNN_train,
-                               predictions_RF_train, predictions_AB_train, predictions_GNB_train), axis=1)
+                               predictions_RF_train, predictions_AB_train, predictions_GNB_train, predictions_RFR_train), axis=1)
 
 
 meta_boost = xgb.XGBClassifier(max_depth=2, learning_rate=0.05, n_estimators=100)
@@ -206,6 +235,7 @@ x_test = combine[1].drop(['Unnamed: 0'], axis=1)
 predictions_LR_train = logreg.predict_proba(x_test)[:,1]
 predictions_DT_train = dt.predict_proba(x_test)[:,1]
 predictions_NN_train = nn.predict_proba(x_test)[:,1]
+predictions_RFR_train = rfreg.predict(x_test)
 predictions_KNN_train = knn.predict_proba(x_test)[:,1]
 predictions_RF_train = rf.predict_proba(x_test)[:,1]
 predictions_AB_train = ab.predict_proba(x_test)[:,1]
@@ -215,13 +245,14 @@ predictions_GNB_train = gnb.predict_proba(x_test)[:,1]
 predictions_LR_train = predictions_LR_train.reshape(-1, 1)
 predictions_DT_train = predictions_DT_train.reshape(-1, 1)
 predictions_NN_train = predictions_NN_train.reshape(-1, 1)
+predictions_RFR_train = predictions_RFR_train.reshape(-1, 1)
 predictions_KNN_train = predictions_KNN_train.reshape(-1, 1)
 predictions_RF_train = predictions_RF_train.reshape(-1, 1)
 predictions_AB_train = predictions_AB_train.reshape(-1, 1)
 predictions_GNB_train = predictions_GNB_train.reshape(-1, 1)
 
 next_x_test = np.concatenate((predictions_LR_train,predictions_DT_train, predictions_NN_train, predictions_KNN_train,
-                               predictions_RF_train, predictions_AB_train, predictions_GNB_train), axis=1)
+                               predictions_RF_train, predictions_AB_train, predictions_GNB_train, predictions_RFR_train), axis=1)
 
 
 # Make predictions on the test set with the final model
@@ -229,6 +260,11 @@ final_pred = meta_boost.predict_proba(next_x_test)[:,1]
 
 sub = pd.DataFrame({'Id': combine[1]['Unnamed: 0'], 'Probability': final_pred})
 sub.to_csv("Credit_Risk.csv", index=False)
+
+
+
+
+
 
 
 
